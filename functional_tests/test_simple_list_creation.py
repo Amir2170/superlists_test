1d607976186fadbb829bad7d3614dd22aee779e2
Amir2170 <amirrezaamp2170@gmail.com>
2020-09-28 10:07:50 -0700

    rename all item input ids and names. still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -17,7 +17,7 @@ class NewVisitorTest(FunctionalTest):
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('to-do', header_text)
 		#user is invited to enter a to-do list straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		self.assertEqual(inputbox.get_attribute('placeholder'), 
 		'Enter a to-do item')
 		#user types smt into text box
@@ -30,7 +30,7 @@ class NewVisitorTest(FunctionalTest):
 		#The page updates and there is a place holder still
 		#inviting the user to enter a message user enter another 
 		#message and press 'ENTER' the page updates and shows both items
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -39,7 +39,7 @@ class NewVisitorTest(FunctionalTest):
 	def test_multiple_users_can_start_lists_at_different_url(self):
 		#user starts a new to do list
 		self.browser.get(self.live_server_url)
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -63,7 +63,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertNotIn('make a fly', page_text)
         
 		#francis starts a new list by entering a new item
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
