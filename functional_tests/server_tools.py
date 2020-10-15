e899e991abd485a5da34a74334201a997f80ccf0
Amir2170 <amirrezaamp2170@gmail.com>
2020-10-14 21:15:44 -0700

    changed an error in test_my_lists

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -7,7 +7,7 @@ def  _get_manage_dot_py(host):
 def reset_database(host):
 	manage_dot_py = _get_manage_dot_py(host)
 	with settings(host_string=f'elspeth@{host}'):
-		run(f'{manage_dot_by} flush --noinput')
+		run(f'{manage_dot_py} flush --noinput')
 
 def create_session_on_server(host, email):
 	manage_dot_py = _get_manage_dot_py
